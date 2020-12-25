fca949f24c4593f1d6e6dc35bbddc076d9040204
Shin Yeong Chan <syc1488@naver.com>
2020-12-26 07:41:39 +0900

    init commit

---
+++ app/src/main/java/com/shin/kart/ui/activity/MainActivity.java
@@ -0,0 +1,18 @@
+package com.shin.kart.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.shin.kart.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
