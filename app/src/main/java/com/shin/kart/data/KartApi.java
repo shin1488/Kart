d1a829bcc4267c2e50a014d64da737452e2bde9f
Shin Yeong Chan <syc1488@naver.com>
2021-01-01 10:29:45 +0900

    Add api request

---
+++ app/src/main/java/com/shin/kart/data/KartApi.java
@@ -0,0 +1,93 @@
+package com.shin.kart.data;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.Log;
+
+import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
+
+import com.shin.kart.R;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+public class KartApi {
+
+    public Context context;
+    public String userName;
+    public String responseBody;
+
+    public KartApi (String userName, Context context) {
+        this.userName = userName;
+        this.context = context;
+    }
+
+    public void main() {
+
+        String apiKey = context.getResources().getString(R.string.api_key);
+        String apiURL = "https://api.nexon.co.kr/kart/v1.0/users/nickname/" + userName;
+
+        Map<String, String> requestHeaders = new HashMap<>();
+        requestHeaders.put("Authorization", apiKey);
+        responseBody = get(apiURL,requestHeaders);
+        Log.d("Rp", responseBody);
+    }
+
+    private String get(String apiUrl, Map<String, String> requestHeaders){
+        HttpURLConnection con = connect(apiUrl);
+        try {
+            con.setRequestMethod("GET");
+            for(Map.Entry<String, String> header :requestHeaders.entrySet()) {
+                con.setRequestProperty(header.getKey(), header.getValue());
+            }
+
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 호출
+                return readBody(con.getInputStream());
+            } else { // 에러 발생
+                return readBody(con.getErrorStream());
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException("API 요청과 응답 실패", e);
+        } finally {
+            con.disconnect();
+        }
+    }
+
+    private HttpURLConnection connect(String apiUrl){
+        try {
+            URL url = new URL(apiUrl);
+            return (HttpURLConnection)url.openConnection();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("API URL이 잘못되었습니다. : " + apiUrl, e);
+        } catch (IOException e) {
+            throw new RuntimeException("연결이 실패했습니다. : " + apiUrl, e);
+        }
+    }
+
+    private String readBody(InputStream body){
+        InputStreamReader streamReader = new InputStreamReader(body);
+
+        try (BufferedReader lineReader = new BufferedReader(streamReader)) {
+            StringBuilder responseBody = new StringBuilder();
+
+            String line;
+            while ((line = lineReader.readLine()) != null) {
+                responseBody.append(line);
+            }
+
+            return responseBody.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("API 응답을 읽는데 실패했습니다.", e);
+        }
+    }
+}
--- app/src/main/java/com/shin/kart/ui/activity/MainActivity.java
+++ app/src/main/java/com/shin/kart/ui/activity/MainActivity.java
@@ -2,17 +2,46 @@ package com.shin.kart.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.shin.kart.R;
+import com.shin.kart.data.KartApi;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button getButton;
+    public EditText editText;
+    public String userData;
+
+    public Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        context = this;
 
+        getButton = (Button)findViewById(R.id.button);
+        editText = (EditText)findViewById(R.id.edittext);
+
+        getButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Thread thread = new Thread() {
+                    public void run() {
+                        KartApi api = new KartApi(editText.getText().toString(), context);
+                        api.main();
+                        userData = api.responseBody;
+                    }
+                };
+                thread.start();
+            }
+        });
     }
 }
